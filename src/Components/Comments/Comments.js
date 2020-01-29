3fefb6e21a1125531dbec383a55617c24e0ef537
mliraudcoel <melanie.liraud@gmail.com>
2020-01-29 17:41:50 +0100

    form component is ok

--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { apiEndPoint } from '../../config';
+import axios from 'axios';
 
 const Comments = () => {
 
+  const [form, setForm] = useState({
+    pseudo:'',
+    comment:''
+  });
+
+  const addComment = (event) => {
+    event.preventDefault();
+     axios.post(`${apiEndPoint}/comments`, {
+       pseudo: form.pseudo,
+       comment: form.comment,
+     });
+     console.log('ok')
+    };
+
   return(
-    <h2>Comments</h2>
+    <>
+      <h2>Comments</h2>
+      <form>
+      <label htmlFor="h4 mb-4"> Votre pseudo </label>
+      <div className="container-input">
+        <input type="text" id="defaultContactFormName" onChange={(event) => setForm({...form, pseudo: event.target.value})} value={form.pseudo} />
+      </div>
+        <label htmlFor="h4 mb-4"> Votre commentaire </label>
+      <div className="container-input">
+        <input type="text" className="input-comment" id="defaultContactFormName" onChange={(event) => setForm({...form, comment: event.target.value})} value={form.comment} />
+      </div>
+      <div className="container-input">
+        <button onClick={addComment}>Envoyer</button>
+      </div>
+    </form>
+  </>
   );
 };
 
