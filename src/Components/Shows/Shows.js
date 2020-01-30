8ed17c238eacb82241abf6e0d1a8b69d85ad5a92
mliraudcoel <melanie.liraud@gmail.com>
2020-01-30 10:46:34 +0100

    change date format working

--- src/Components/Shows/Shows.js
+++ src/Components/Shows/Shows.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { apiEndPoint } from '../../config';
+import './Shows.css';
 
 const Shows = () => {
 
@@ -13,21 +14,34 @@ const Shows = () => {
     axios.get(`${apiEndPoint}/shows`)
     .then(response => {
       console.log(response.data);
+      // setShows(response.data);
       setShows(response.data);
     })
   },[]);
 
+  console.log(shows[0].date);
+
+  const changeDate = (dateToChange) => {
+    let date = new Date(dateToChange);
+    console.log(date);
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    console.log(date.toLocaleDateString('fr-FR', options));
+    return date.toLocaleDateString('fr-FR', options);
+  };
+  
   return(
     <>
       <h2>Shows</h2>
+      <select>
+      </select>
       <div className="list">
       {shows.map((show, index) => {
         return(
           <div key={index}>
-            <ul>
+            <ul className="shows">
               <li>
                 <h3>{show.city}</h3>
-                <p>{show.date}</p>
+                <p>{changeDate(show.date)}</p>
               </li>
             </ul>
           </div>
