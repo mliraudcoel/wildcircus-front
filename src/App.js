2b97c55bbd385d22ceee0f16a08949544771f500
mliraudcoel <melanie.liraud@gmail.com>
2020-01-30 15:19:27 +0100

    adding and getting spot in admin working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home/Home';
 import Admin from './Components/Admin/Admin';
 
--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {apiEndPoint} from '../../config';
 
 const Admin = () => {
 
+  const [form, setForm] = useState({
+    city:'',
+    date:''
+  });
+
+  const [shows, setShows] = useState([{
+    city:'',
+    date:''
+  }]);
+
+  useEffect(() => {
+    axios.get(`${apiEndPoint}/shows`)
+    .then(response => {
+      console.log(response.data);
+      setShows(response.data);
+    })
+  },[]);
+
+  const addShow = (event) => {
+    event.preventDefault();
+     axios.post(`${apiEndPoint}/shows`, {
+       city: form.city,
+       date: form.date,
+     });
+     console.log('ok')
+    };
+
+  const changeDate = (dateToChange) => {
+    let date = new Date(dateToChange);
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return date.toLocaleDateString('fr-FR', options);
+  };
+
   return(
-    <div>Admin</div>
+    <>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">City</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(event) => setForm({...form, city: event.target.value})} value={form.city}/>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Date</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" placeholder="2020-04-12 20:30:00" onChange={(event) => setForm({...form, date: event.target.value})} value={form.date}/>
+        </div>
+        <button type="submit" class="btn btn-primary" onClick={addShow}>Submit</button>
+      </form>
+      {shows.map((show, index) => {
+        return(
+          <div key={index}>
+            <ul className="shows">
+              <li>
+                <h3>{show.city}</h3>
+                <p>{changeDate(show.date)}</p>
+              </li>
+            </ul>
+          </div>
+        );
+      })
+      }
+    </>
   )
 }
 
