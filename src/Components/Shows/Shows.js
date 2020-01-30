c0dbdb1a6c78646fa88b7d4346ae8099f53babe1
mliraudcoel <melanie.liraud@gmail.com>
2020-01-30 09:35:46 +0100

    get shows working

--- src/Components/Shows/Shows.js
+++ src/Components/Shows/Shows.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { apiEndPoint } from '../../config';
 
 const Shows = () => {
 
+  const [shows, setShows] = useState([{
+    city:'',
+    date:''
+  }]);
+
+  useEffect(() => {
+    axios.get(`${apiEndPoint}/shows`)
+    .then(response => {
+      console.log(response.data);
+      setShows(response.data);
+    })
+  },[]);
+
   return(
-    <h2>Shows</h2>
-  )
-}
+    <>
+      <h2>Shows</h2>
+      <div className="list">
+      {shows.map((show, index) => {
+        return(
+          <div key={index}>
+            <ul>
+              <li>
+                <h3>{show.city}</h3>
+                <p>{show.date}</p>
+              </li>
+            </ul>
+          </div>
+        );
+      })
+      }
+      </div>
+    </>
+  );
+  };
 
 export default Shows;
